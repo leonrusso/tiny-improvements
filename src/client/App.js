2532d562ae55155d3c67ba4974e92b9f9fe5edd6
Uncle B <leonurusso@gmail.com>
2018-07-18 19:55:39 -0400

    Day 4 - Props

--- src/client/App.js
+++ src/client/App.js
@@ -1,32 +1,92 @@
 import React, { Component } from "react";
-import { Col, Container, Row, Button} from "reactstrap";
+import { Col, Container, Row, Form, FormGroup, Input, Label, Card, CardBody, Button } from "reactstrap";
+import AwardCard from "./components/AwardCard";
 
-class App extends Component { 
+class App extends Component {
 
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col md="12">
-            <h1>Tiny Progress</h1>
-          </Col>
-        </Row>
-        <br />
-        <Row>
-          <Col md="12" lg="3">
-            <Button color="success">Give Kudos</Button>
-          </Col>
-          <Col md="12" lg="9">
-            <img alt="award" src="http://www.pngmart.com/files/3/Award-PNG-Photos.png" width="50px" />
-            <p>Badge Name</p>
-            <img alt="avatar" src="https://www.iranketab.ir/Images/user.jpg" width="100px" />
-            <h2> Heading </h2>
-            <p>Conversion stealth influencer business-to-business entrepreneur hypotheses investor customer deployment metrics learning curve direct mailing long tail mass market. Pitch iteration stock android business-to-consumer bandwidth seed round user experience paradigm shift channels equity pivot. Metrics partner network validation responsive web design first mover advantage backing research & development market mass market innovator sales infrastructure.</p>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            users: [
+                {
+                    userId: 45089,
+                    name: "Owen",
+                    position: "Captian of the Breakroom"
+                },
+                {
+                    userId: 223,
+                    name: "Brooke",
+                    position: "Winner of All Dance-Offs"
+                },
+                {
+                    userId: 6582,
+                    name: "Gobi",
+                    position: "King of Mid-Day Naps"
+                }
+            ],
+            awards: [
+                {
+                    id: 1,
+                    title: "Best Boss Award!",
+                    comment: "Thanks for always looking out for us."
+                },
+                {
+                    id: 2,
+                    title: "Longest Commute Award!",
+                    comment: "I can't believe Leslie makes it to work as often as she does."
+                },
+                {
+                    id: 3,
+                    title: "Most likely to nap at work!",
+                    comment: "Maybe you need more coffee."
+                }
+
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col md="12">
+                        <h1>Tiny Progress</h1>
+                    </Col>
+                </Row>
+                <br />
+                <Row>
+                    <Col md="12" lg="3">
+                        <Card>
+                            <CardBody className="mx-auto">
+                                <Button color="success">Give Kudos</Button>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col md="12" lg="9">
+                        {this.state.awards.map(element => <AwardCard title={element.title} comment={element.comment} />)}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md="12">
+                        <Form>
+                            <FormGroup>
+                                <Label>Give Kudos to</Label>
+                                <Input type="select">
+                                    {this.state.users.map(element => <option>{element.name}</option>)}
+                                </Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Input type="text" placeholder="Kudos Title" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Input type="textarea" placeholder="Kudos text" />
+                            </FormGroup>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
 }
 
 export default App;
